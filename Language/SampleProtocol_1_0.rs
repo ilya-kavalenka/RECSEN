9927ea9d48f281dd4060dba8d53d5282a3c0c1e7
Ilya <ilya.kavalenka@gmail.com>
2017-04-20 23:20:37 +0300

    sample protocol

---
+++ Language/SampleProtocol_1_0.rs
@@ -0,0 +1,38 @@
+/*
+    The basic protocol
+*/
+
+protocol SampleProtocol(1.0)
+{
+    enum Side
+    {
+        Bid,
+        Ask
+    }
+
+    group SnapshotRefreshEntry
+    {
+        Side Side;
+        int32 Qty;
+        double Price;
+        int32 Orders;
+    }
+
+    message SnapshotRefresh
+    {
+        string Symbol;
+        SnapshotRefreshEntry[] Entries;
+    }
+
+    processor Client()
+    {
+        recv onSnapshot(SnapshotRefresh)
+            repeat;
+    }
+
+    processor Server()
+    {
+        send (SnapshotRefresh)
+            repeat;
+    }
+}
